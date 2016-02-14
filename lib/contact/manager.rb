8382ef834da01bb3ef701788175d0afe3c093661
Dane O'Connor <dane.oconnor@gmail.com>
2016-02-14 13:03:44 -0500

    Generate gem

---
+++ lib/contact/manager.rb
@@ -0,0 +1,7 @@
+require "contact/manager/version"
+
+module Contact
+  module Manager
+    # Your code goes here...
+  end
+end
---
+++ lib/contact/manager/version.rb
@@ -0,0 +1,5 @@
+module Contact
+  module Manager
+    VERSION = "0.1.0"
+  end
+end
